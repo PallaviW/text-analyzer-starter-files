fba6c096605106cadc2b7a4738f500a2463c910d
Pallvi Walali <beyonce@thinkful.comwalali.pallavi@gmail.com>
2017-11-09 16:33:58 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // your code here!
+
+$('.js-form').submit(function(event){
+
+ event.preventDefault();
+
+ var userTextElement = $(event.currentTarget).find('#user-text').val().toLowerCase();
+ console.log(userTextElement);
+	
+var totalWords =[];
+
+totalwords = userTextElement.split(" ");
+
+var wordCount = totalwords.length;
+
+var uniqueArray =[];
+
+totalwords.forEach(function(item){
+
+	if(uniqueArray.indexOf(item) === -1){
+		uniqueArray.push(item);
+	}
+
+});
+
+var uniqueWordCount = uniqueArray.length;
+
+var result = (userTextElement.split(" ").join("").length) / wordCount;
+
+var avgWordLength = result.toFixed(2);
+
+
+$('.js-display').removeClass('hidden');
+$('.js-wordcount').text(wordCount);
+$('.js-unique').text(uniqueWordCount);
+$('.js-avg').text(avgWordLength);
+
+});
